5015e26e6e1f8f0505171be8ea63991d0bb4fa63
Sebastian Binder <designstudio@sebastianbinder.com>
2014-01-01 20:12:37 +0100

    fix

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,5 +1,9 @@
-customer1 = Customer.create(
-  name: "MyClub",
+user1 = User.create(
+  password: "test12345",
+  firstname: "Taylor",
+  lastname: "Class",
+  email: "tclass@sebastianbinder.com",
+  phone_number: "+1-647-3746",
   street: "Brooklyn Street",
   street_number: "1460",
   zipcode: "10024",
