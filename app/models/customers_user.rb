b26f29e2dc6496b5983281246821d9aaf06e227a
Sebastian Binder <designstudio@sebastianbinder.com>
2014-01-02 14:00:40 +0100

    fix users

--- app/models/customers_user.rb
+++ app/models/customers_user.rb
@@ -6,6 +6,8 @@ class CustomersUser < ActiveRecord::Base
 	
 	  
 ROLES = %w[viewer member organizer customer admin]
+ROLES_SELECTABLE = %w[viewer member organizer customer]
+
 def role?(base_role)
   role.present? && ROLES.index(base_role.to_s) <= ROLES.index(role)
 end
