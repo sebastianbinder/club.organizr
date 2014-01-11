480a374d2a6db42894204b142e209dd6b4ca10be
Sebastian Binder <designstudio@sebastianbinder.com>
2014-01-11 15:14:01 +0100

    add indices - imprint, privacy

---
+++ app/controllers/indices_controller.rb
@@ -0,0 +1,4 @@
+class IndicesController < ApplicationController
+	def imprint
+	end
+end
---
+++ test/controllers/indices_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IndicesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
