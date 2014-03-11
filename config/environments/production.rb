96ee202dc94f6b405d39b009ae852477fa81fd1d
Sebastian Binder <designstudio@sebastianbinder.com>
2014-03-11 15:28:57 +0100

    environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ ClubOrganizr::Application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -27,7 +27,7 @@ ClubOrganizr::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
