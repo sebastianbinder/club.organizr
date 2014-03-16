a9b464d55f12af49ed725874685008c4f5cc050a
Sebastian Binder <designstudio@sebastianbinder.com>
2014-03-16 14:46:30 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module ClubOrganizr
     
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
     
   end
 end
